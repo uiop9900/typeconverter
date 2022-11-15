b2d8d82615bb8da79d059f1954f65b764ee14567
jiaLEE <jialee@marketboro.com>
2022-11-15 20:06:52 +0900

    spring - conversionService.

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,12 @@ public class HelloController {
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "ok";  
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) { // 쿼리스트링으로 String 으로 보냈는데, 알아서 객체로 받음
+        System.out.println("ipPort = IP" + ipPort.getIp());
+        System.out.println("ipPort = PORT" + ipPort.getPort());
+        return "ok";
     }
 }
